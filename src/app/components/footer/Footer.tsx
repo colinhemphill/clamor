29a629faf96b599098709dd4f0f4d6f2f73a2c03
Colin Hemphill <colin.hemphill@bit.ly>
2023-07-12 11:01:31 -0500

    Add edge config.

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,27 +1,35 @@
 import { Alert, AlertDescription } from '@/components/ui/alert';
+import { get } from '@vercel/edge-config';
 import { buttonVariants } from '../ui/button';
 import AboutDialog from './AboutDialog';
 import OpenSourceDialog from './OpenSourceDialog';
 
-export default function Footer() {
+export default async function Footer() {
+  const macDownload = await get('tauri-desktop-mac-download');
+  const windowsDownload = await get('tauri-desktop-windows-download');
+
   return (
     <footer className="mt-8 flex flex-col gap-2">
       <Alert>
         <AlertDescription className="flex flex-col gap-2 text-sm text-muted-foreground sm:flex-row sm:items-center">
           <div className="flex-grow">Download for desktop</div>
 
-          <a
-            className={buttonVariants({ size: 'sm', variant: 'secondary' })}
-            href=""
-          >
-            Mac
-          </a>
-          <a
-            className={buttonVariants({ size: 'sm', variant: 'secondary' })}
-            href=""
-          >
-            Windows
-          </a>
+          {macDownload && (
+            <a
+              className={buttonVariants({ size: 'sm', variant: 'secondary' })}
+              href=""
+            >
+              Mac
+            </a>
+          )}
+          {windowsDownload && (
+            <a
+              className={buttonVariants({ size: 'sm', variant: 'secondary' })}
+              href=""
+            >
+              Windows
+            </a>
+          )}
         </AlertDescription>
       </Alert>
       <Alert>
