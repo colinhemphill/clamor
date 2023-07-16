d26295725ae44bdcf76e23d046f06873fe42d668
Colin Hemphill <colin.hemphill@bit.ly>
2023-07-16 09:45:55 -0500

    Cleanup.

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,8 +1,1 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
 export const IS_TAURI = process.env.NEXT_PUBLIC_DESKTOP_BUILD === 'tauri';
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
