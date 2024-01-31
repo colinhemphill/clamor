13eec13064d25be3534dd8aecd756194e128fe9b
Colin Hemphill <colin.hemphill@bit.ly>
2024-01-31 16:45:18 -0600

    Fix button child rendering.

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -42,7 +42,7 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, children, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
