ad1b53bac01f8c84f1bdf5bcfeb0efe5c2ffd04d
Colin Hemphill <colin.hemphill@bit.ly>
2024-11-08 15:15:37 -0600

    Updates. Remove edge config

--- src/app/components/footer/Downloads.tsx
+++ src/app/components/footer/Downloads.tsx
@@ -2,39 +2,24 @@
 
 import { Alert, AlertDescription } from '@/app/components/ui/alert';
 import { buttonVariants } from '@/app/components/ui/button';
-import { Skeleton } from '@/app/components/ui/skeleton';
-import useDownloads from '@/lib/hooks/useDownloads';
 
 export default function Downloads() {
-  const { downloads, isLoading } = useDownloads();
-
   return (
     <Alert>
       <AlertDescription className="flex flex-col items-center gap-2 text-sm text-muted-foreground md:flex-row">
         <div className="flex-grow">Download for desktop</div>
-
-        {(!downloads || isLoading) && (
-          <>
-            <Skeleton className="h-8 w-[48px] rounded-md" />
-            <Skeleton className="h-8 w-[77px] rounded-md" />
-          </>
-        )}
-        {downloads?.mac && (
-          <a
-            className={buttonVariants({ size: 'sm', variant: 'secondary' })}
-            href={downloads.mac}
-          >
-            Mac
-          </a>
-        )}
-        {downloads?.windows && (
-          <a
-            className={buttonVariants({ size: 'sm', variant: 'secondary' })}
-            href={downloads.windows}
-          >
-            Windows
-          </a>
-        )}
+        <a
+          className={buttonVariants({ size: 'sm', variant: 'secondary' })}
+          href="https://github.com/colinhemphill/clamor/releases/download/1.0.0/Clamor.Studio_1.0.0_aarch64.dmg"
+        >
+          Mac
+        </a>
+        <a
+          className={buttonVariants({ size: 'sm', variant: 'secondary' })}
+          href="https://github.com/colinhemphill/clamor/releases/download/1.0.0/Clamor.Studio_1.0.0_x64_en-US.msi"
+        >
+          Windows
+        </a>
       </AlertDescription>
     </Alert>
   );
